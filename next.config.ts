f41ed6287532c5851a6d325850edce18099fdec7
sujata774 <gajmersujata23@gmail.com>
2025-06-23 12:17:04 +0600

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'miro.medium.com',
+      'm.media-amazon.com',
+      'www.eventact.com'
+    ],
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
